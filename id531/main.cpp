73a2d70c6edc00e174839df10422d73927211d7d
Alejandro <alejandroaigner1999@hotmail.com>
2023-10-03 21:15:14 -0400

    [IMP]: Base functions

--- id531/main.cpp
+++ id531/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 
 using namespace std;
+
+float get_porcentage(float subset, float omega)
+{
+    return subset*100/omega;
+}
+
+float rule_of_three(float alpha, float beta, float epsilon)
+{
+    return beta*epsilon/alpha;
+}
+
 int main()
 {
-    cout << "Machete machete!!" << endl;
+    float x = get_porcentage(30,59.4);
+    cout << x << endl;
     return 0;
 }
